06911dcf2856f46d52dc85fc3bbc33d0fe45a0c0
Everton Zoelner <everton.zoelner@gmail.com>
2019-04-03 11:17:13 -0300

    CHANGE Routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import Header from "../containers/Header";
+import Content from "../components/Content";
+import MainPage from "../pages/MainPage";
+
+const DefaultLayout = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={matchProps => (
+      <>
+        <Header />
+        <Content>
+          <Component {...matchProps} />
+        </Content>
+        <div className="Footer" />
+      </>
+    )}
+  />
+);
+
+export default function Routes() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <DefaultLayout exact path="/" component={MainPage} />
+      </Router>
+    </Provider>
+  );
+}
