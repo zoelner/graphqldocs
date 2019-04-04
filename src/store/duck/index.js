95ecf5380bfcfc94a0d5477b7bd5d948db0c954a
Everton Zoelner <everton.zoelner@gmail.com>
2019-04-04 14:54:49 -0300

    ADD Adicionado reducer navigation

--- src/store/duck/index.js
+++ src/store/duck/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import schema from "./schema";
+import navigation from "./navigation";
 
 export const reducers = combineReducers({
-  schema
+  schema,
+  navigation
 });
