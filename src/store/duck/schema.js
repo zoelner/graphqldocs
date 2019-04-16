7c8f0bbf3226177ccdc9ba7fec241da73b97c571
Everton Zoelner <everton.zoelner@gmail.com>
2019-04-16 10:35:38 -0300

    ADD Duck Schema

--- src/store/duck/schema.js
+++ src/store/duck/schema.js
@@ -1,10 +1,47 @@
-import { data } from "../../__mock__/instrospection";
 import { buildClientSchema } from "graphql";
 
-const INITIAL_STATE = buildClientSchema(data);
+export const Types = {
+  SCHEMA_REQUEST: "@schema/SCHEMA_REQUEST",
+  SCHEMA_REQUEST_FAILED: "@schema/SCHEMA_REQUEST_FAILED",
+  SCHEMA_REQUEST_SUCCESS: "@schema/SCHEMA_REQUEST_SUCCESS"
+};
+
+export const Creators = {
+  schemaRequest: () => ({
+    type: Types.SCHEMA_REQUEST
+  }),
+  schemaRequestFailed: () => ({}),
+  schemaRequestSuccess: payload => ({
+    type: Types.SCHEMA_REQUEST_SUCCESS,
+    payload
+  })
+};
+
+const INITIAL_STATE = {
+  isLoading: true,
+  error: false,
+  data: null
+};
 
 export default function schema(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case Types.SCHEMA_REQUEST:
+      return Object.assign({}, state, {
+        isLoading: true,
+        error: false
+      });
+    case Types.SCHEMA_REQUEST_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        error: false,
+        data: buildClientSchema(action.payload)
+      });
+    case Types.SCHEMA_REQUEST_FAILED:
+      return Object.assign({}, state, {
+        isLoading: false,
+        error: true
+      });
+
     default:
       return state;
   }
