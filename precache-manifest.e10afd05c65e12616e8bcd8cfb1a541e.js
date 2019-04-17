bf67e470c41415f5ba3acf20d047a52c21714839
Everton Zoelner <everton.zoelner@gmail.com>
2019-04-17 16:57:21 -0300

    Updates

---
+++ precache-manifest.e10afd05c65e12616e8bcd8cfb1a541e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d14ebe037cbf1e045d",
+    "url": "/graphqldocs/static/css/main.0b9572a0.chunk.css"
+  },
+  {
+    "revision": "76d14ebe037cbf1e045d",
+    "url": "/graphqldocs/static/js/main.98288ae1.chunk.js"
+  },
+  {
+    "revision": "9fe888727603aa3c5ddc",
+    "url": "/graphqldocs/static/js/runtime~main.3dff4733.js"
+  },
+  {
+    "revision": "1dca1718d6634cb8396b",
+    "url": "/graphqldocs/static/css/2.dee9560e.chunk.css"
+  },
+  {
+    "revision": "1dca1718d6634cb8396b",
+    "url": "/graphqldocs/static/js/2.cf0a734b.chunk.js"
+  },
+  {
+    "revision": "5bf8a3ca0bb6bfb41063cdd672be9ad9",
+    "url": "/graphqldocs/static/media/GraphQL_Logo.5bf8a3ca.svg"
+  },
+  {
+    "revision": "0f7e147d60e58e3304faa382d60355e1",
+    "url": "/graphqldocs/index.html"
+  }
+];
--- src/store/duck/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import schema from "./schema";
-import navigation from "./navigation";
-
-export const reducers = combineReducers({
-  schema,
-  navigation
-});
